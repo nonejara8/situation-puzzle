276e4e9415d1bc6ae9e473649f75c9805ca553ab
nonejara8 <143790253+nonejara8@users.noreply.github.com>
2024-08-29 18:46:47 +0900

    feat: 埋込み型のテキストを作成

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,3 +1,5 @@
+use serenity::all::Timestamp;
+use serenity::builder::{CreateEmbed, CreateEmbedFooter, CreateMessage};
 use serenity::model::channel::Message;
 use serenity::prelude::*;
 use tracing::error;
@@ -46,9 +48,34 @@ pub async fn handle_message(ctx: Context, msg: Message) {
         }
     }
 
-    if msg.content == "!hello" {
-        if let Err(e) = msg.channel_id.say(&ctx.http, "world!").await {
-            error!("Error sending message: {:?}", e);
+    if msg.content == "!embed" {
+        // The create message builder allows you to easily create embeds and messages using a
+        // builder syntax.
+        // This example will create a message that says "Hello, World!", with an embed that has
+        // a title, description, an image, three fields, and a footer.
+        let footer = CreateEmbedFooter::new("This is a footer");
+        let embed = CreateEmbed::new()
+            .title("This is a title")
+            .description("This is a description")
+            .fields(vec![
+                ("This is the first field", "This is a field body", true),
+                ("This is the second field", "Both fields are inline", true),
+            ])
+            .field(
+                "This is the third field",
+                "This is not an inline field",
+                false,
+            )
+            .footer(footer)
+            // Add a timestamp for the current time
+            // This also accepts a rfc3339 Timestamp
+            .timestamp(Timestamp::now());
+        let builder = CreateMessage::new().content("Hello, World!").embed(embed);
+
+        let msg = msg.channel_id.send_message(&ctx.http, builder).await;
+
+        if let Err(why) = msg {
+            println!("Error sending message: {why:?}");
         }
     }
 
