259bab87221f69ed5d3f51a1560a512be71c2f35
nonejara8 <143790253+nonejara8@users.noreply.github.com>
2024-09-18 20:23:44 +0900

    Botの分割

--- src/main.rs
+++ src/main.rs
@@ -93,61 +93,6 @@ impl Bot {
     }
 }
 
-#[async_trait]
-impl EventHandler for Bot {
-    async fn ready(&self, ctx: Context, ready: Ready) {
-        info!("{} is connected!", ready.user.name);
-
-        let commands = vec![
-            CreateCommand::new("play").description("ゲームスタート"), // ゲームスタート
-            CreateCommand::new("join").description("参加"),           // 参加
-            CreateCommand::new("question")
-                .description("質問を送信します")
-                .add_option(
-                    CreateCommandOption::new(
-                        serenity::all::CommandOptionType::String,
-                        "q",
-                        "質問の内容を入力してください",
-                    )
-                    .max_length(100)
-                    .required(true),
-                ),
-            CreateCommand::new("answer")
-                .description("回答を送信します")
-                .add_option(
-                    CreateCommandOption::new(
-                        serenity::all::CommandOptionType::String,
-                        "a",
-                        "回答の内容を入力してください",
-                    )
-                    .max_length(100)
-                    .required(true),
-                ),
-            CreateCommand::new("giveup").description("ゲームを終了します"),
-        ];
-
-        let commands = &self
-            .discord_guild_id
-            .set_commands(&ctx.http, commands)
-            .await
-            .unwrap();
-
-        info!("Registered commands: {:#?}", commands);
-    }
-
-    async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
-        match interaction {
-            Interaction::Command(command) => handle_command(ctx, command, self).await,
-            Interaction::Component(component) => handle_component(ctx, component).await,
-            _ => (),
-        }
-    }
-
-    async fn message(&self, ctx: Context, msg: Message) {
-        handlers::handle_message(ctx, msg).await;
-    }
-}
-
 
 
 
